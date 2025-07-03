83fbae9a95ae74c977a8fb306d74c4cecf5e6b25
Vasudha Kumari <vasudhagupta2003@gmail.com>
2025-07-04 00:41:35 +0530

    Added  the deployed backend link

--- src/pages/MyDocuments.jsx
+++ src/pages/MyDocuments.jsx
@@ -126,7 +126,7 @@ export default function MyDocuments() {
                     <Eye className="w-5 h-5" />
                   </motion.button>
                   <a
-                    href={`http://localhost:5000/uploads/${doc.filename}`}
+                    href={`https://backend-jnwc.onrender.com/uploads/${doc.filename}`}
                     download={doc.originalname}
                     className="bg-[#7c6f57] hover:bg-[#bfa77a] text-white text-sm px-4 py-2 rounded-lg font-semibold shadow transition-all flex items-center justify-center"
                     title="Download"
